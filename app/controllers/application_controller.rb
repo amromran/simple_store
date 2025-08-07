935231063cc565d002f768ceb72630d288abdbf7
irasrive <3mr.3omran@gmail.com>
2025-08-07 18:42:31 +0300

    added Turbo to display products

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  include ActiveStorage::SetCurrent     # only needs to be done once, and only if we’re storing files on disk
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,45 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+Product.delete_all
+product = Product.create(title: 'Product Eins',
+                         description:
+                           %(<p>
+      <em>Description of product Eins etc etc
+    </p>),
+                         price: 33.95)
+
+product.image.attach(io: File.open(
+  Rails.root.join('db', 'images', 'eins.jpg')),
+                     filename: 'eins.jpg')
+
+product.save!
+# . . .
+product = Product.create(title: 'Product Zwei',
+                         description:
+                           %(<p>
+      <em>Description of product Zwei etc etc
+    </p>),
+                         price: 30.95)
+
+product.image.attach(io: File.open(
+  Rails.root.join('db', 'images', 'zwei.jpg')),
+                     filename: 'zwei.jpg')
+
+product.save!
+# . . .
+
+product = Product.create(title: 'Product Drei',
+                         description:
+                           %(<p>
+      <em>Description of product Drei etc etc
+    </p>),
+                         price: 28.95)
+
+product.image.attach(io: File.open(
+  Rails.root.join('db', 'images', 'drei.jpg')),
+                     filename: 'drei.jpg')
+
+product.save!
